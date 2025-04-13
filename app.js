8b84ec714b27300098e94657dd435bf0a8d85ba3
xangel1111 <angel.delgado@tecsup.edu.pe>
2025-04-13 18:55:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const port = '3000';
-const host = '127.0.0.1';
+const host = '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
